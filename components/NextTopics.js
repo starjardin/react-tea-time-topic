ab52f071ddfb25ed96b3c1c21b01b884fb664d17
Tantely Andrianarivola <tantely.and@onja.org>
2020-10-24 11:07:21 +0300

    added newt topics and its functions

--- components/NextTopics.js
+++ components/NextTopics.js
@@ -1,30 +1,39 @@
 import React, { useState } from 'react'
 import ArchiveButton from './ArchiveButton'
 
-export default function NextTopics ({ topic}) {
+export default function NextTopics ({ 
+  topic,
+  topics,
+  setTopics
+  }) {
 
-  const [likeTopic, setLikeTopic] = useState(0)
-  const [unLikeTopic, setUnLikeTopic] = useState(0)
 
-  function increment () {
-    setLikeTopic(prev => prev + 1)
+  function upvotesIncrement () {
+    topic.upvotes ++
+    setTopics([...topics])
   }
   
-  function decrement () {
-    setUnLikeTopic(prev => prev + 1)
+  function downvotesDecrement () {
+    topic.downvotes ++
+    setTopics([...topics])
   }
+
   return  (
     <>
       <div className="next-topic">
         <div className="row">
           <p>{topic.title}</p>
-          <ArchiveButton />
+          <ArchiveButton 
+            topic={topic}
+            topics={topics}
+            setTopics={setTopics}
+          />
         </div>
         <div>
-          <button onClick={increment}>+👍</button>
-          <span>{likeTopic}</span>
-          <button onClick={decrement}>-👎</button>
-          <span>{unLikeTopic}</span>
+          <button onClick={upvotesIncrement}>+👍</button>
+          <span>{topic.upvotes}</span>
+          <button onClick={downvotesDecrement}>-👎</button>
+          <span>{topic.downvotes}</span>
         </div>
       </div>
     </>
