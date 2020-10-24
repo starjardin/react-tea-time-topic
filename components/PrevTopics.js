f94b4a131d317d6bc86eaba1f08d4d99e53f26f5
Tantely Andrianarivola <tantely.and@onja.org>
2020-10-24 11:06:49 +0300

    added previous topics and its functions

--- components/PrevTopics.js
+++ components/PrevTopics.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import DeleteButton from './DeleteButton'
 
-export default function PrevTopics ({ topic }) {
+export default function PrevTopics ({ 
+  topic,
+  prevTopics,
+  setPrevTopics
+  }) {
   return (
     <>
       <div className="prev-topic">
         <div className="row">
           <p>{topic.title}</p>
-          <DeleteButton />
+          <DeleteButton 
+            topic={topic}
+            prevTopics={prevTopics}
+            setPrevTopics={setPrevTopics} 
+          />
         </div>
       </div>
     </>
