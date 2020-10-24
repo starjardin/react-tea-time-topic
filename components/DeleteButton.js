12500c707193f71e3fe7fe7decaf66aae17d563b
Tantely Andrianarivola <tantely.and@onja.org>
2020-10-24 11:03:23 +0300

    added delete function

--- components/DeleteButton.js
+++ components/DeleteButton.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 
-export default function DeleteButton () {
+export default function DeleteButton ({ 
+  // destructuring objects i.e. topic= topic.topic
+  topic,
+  prevTopics,
+  setPrevTopics
+}) {
+
+//This is the id of the element to delete
+const id = topic.id
+//function to delete an item (a topic)
+  function deleted () {
+    setPrevTopics(prevTopics.filter(prevTopic => prevTopic.id !== id))
+  }
   return (
     <>
-      <button>Delete</button>
+      <button
+        onClick={deleted}
+      >Delete</button>
     </>
   )
 }
